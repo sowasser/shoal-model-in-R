a0e7f94a76eedfb57d0e20f93d3e69c673ed59c5
Sophia Wassermann <so.wassermann@gmail.com>
2017-11-16 14:22:11 +0000

    Changed colors of graphs for BES poster
    Still need to change axis ticks, background color (maybe transparent?), and font.

--- SA_graphs.R
+++ SA_graphs.R
@@ -14,82 +14,88 @@ colnames(num_50) <- columns
 num_200 <- read.csv("shoal_data_200.csv")
 colnames(num_200) <- columns
 
+# Colors for BES poster
+# Cream: #FEF9F3
+# Dark blue: #1B4460
+# Light blue: #0A7794
+# Burgundy: #6F115D
+
 
 # Create graphs for nearest neighbour distance and polarization for each
 # variation of agent number (50, 100, 200).
 nnd50 <- ggplot() + 
   theme_bw() + 
-  geom_line(data=num_50, aes(x=step, y=nnd), colour="blue", size = 1) +  # line
+  geom_line(data=num_50, aes(x=step, y=nnd), colour="#0A7794", size = 1) +  # line
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
-  theme(axis.text.y = element_text(size = 10),  # axis text size
-        axis.text.x = element_text(size = 10)) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
   xlab("step") + ylab("Nearest Neighbour Distance") +  # axis labels
   ggtitle("n = 50") +  # chart title text, left justified
   theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 12, face = 'bold')) +   # label text
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold')) +   # label text
   theme(panel.border = element_rect(colour = 'white',size = 1))  # no border (white border)
 polar50 <- ggplot() + 
   theme_bw() + 
-  geom_line(data=num_50, aes(x=step, y=polar), colour="red", size = 1) +  # line
+  geom_line(data=num_50, aes(x=step, y=polar), colour="#6F115D", size = 1) +  # line
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
-  theme(axis.text.y = element_text(size = 10),  # axis text size
-        axis.text.x = element_text(size = 10)) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
   xlab("step") + ylab("Polarization") +  # axis labels
   ggtitle("n = 50") +  # chart title text, left justified
   theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 12, face = 'bold')) +   # label text
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold')) +   # label text
   theme(panel.border = element_rect(colour = 'white',size = 1))  # no border (white border)
 
 nnd100 <- ggplot() + 
   theme_bw() + 
-  geom_line(data=num_100, aes(x=step, y=nnd), colour="blue", size = 1) +  # line
+  geom_line(data=num_100, aes(x=step, y=nnd), colour="#0A7794", size = 1) +  # line
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
-  theme(axis.text.y = element_text(size = 10),  # axis text size
-        axis.text.x = element_text(size = 10)) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
   xlab("step") + ylab("Nearest Neighbour Distance") +  # axis labels
   ggtitle("n = 100") +  # chart title text, left justified
   theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 12, face = 'bold')) +   # label text
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold')) +   # label text
   theme(panel.border = element_rect(colour = 'white',size = 1))  # no border (white border)
 polar100 <- ggplot() + 
   theme_bw() + 
-  geom_line(data=num_100, aes(x=step, y=polar), colour="red", size = 1) +  # line
+  geom_line(data=num_100, aes(x=step, y=polar), colour="#6F115D", size = 1) +  # line
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
-  theme(axis.text.y = element_text(size = 10),  # axis text size
-        axis.text.x = element_text(size = 10)) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
   xlab("step") + ylab("Polarization") +  # axis labels
   ggtitle("n = 100") +  # chart title text, left justified
   theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 12, face = 'bold')) +   # label text
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold')) +   # label text
   theme(panel.border = element_rect(colour = 'white',size = 1))  # no border (white border)
 
 nnd200 <- ggplot() + 
   theme_bw() + 
-  geom_line(data=num_200, aes(x=step, y=nnd), colour="blue", size = 1) +  # line
+  geom_line(data=num_200, aes(x=step, y=nnd), colour="#0A7794", size = 1) +  # line
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
-  theme(axis.text.y = element_text(size = 10),  # axis text size
-        axis.text.x = element_text(size = 10)) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
   xlab("step") + ylab("Nearest Neighbour Distance") +  # axis labels
   ggtitle("n = 200") +  # chart title text, left justified
   theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 12, face = 'bold')) +   # label text
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold')) +   # label text
   theme(panel.border = element_rect(colour = 'white',size = 1))  # no border (white border)
 polar200 <- ggplot() + 
   theme_bw() + 
-  geom_line(data=num_200, aes(x=step, y=polar), colour="red", size = 1) +  # line
+  geom_line(data=num_200, aes(x=step, y=polar), colour="#6F115D", size = 1) +  # line
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +  # no gridlines
-  theme(axis.text.y = element_text(size = 10),  # axis text size
-        axis.text.x = element_text(size = 10)) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
   xlab("step") + ylab("Polarization") +  # axis labels
   ggtitle("n = 200") +  # chart title text, left justified
   theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 12, face = 'bold')) +   # label text
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold')) +   # label text
   theme(panel.border = element_rect(colour = 'white',size = 1))  # no border (white border)
 
 # Call multiplot function - located in seperate file. Orders plot column by column
