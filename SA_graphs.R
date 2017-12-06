31cbbc5f8e55567a0db13cf6d113687cbd817a79
Sophia Wassermann <so.wassermann@gmail.com>
2017-12-06 12:13:59 +0000

    Added code for shoal area graphs

--- SA_graphs.R
+++ SA_graphs.R
@@ -91,3 +91,42 @@ polar200 <- ggplot() +
 
 # Call multiplot function - located in seperate file. Orders plot column by column
 multiplot(nnd50, nnd100, nnd200, polar50, polar100, polar200, cols=2)
+
+
+# Create graphs for shoal area (convex hull)
+area50 <- ggplot() + 
+  theme_classic() + 
+  geom_line(data=num_50, aes(x=step, y=area), colour="#6F115D", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
+  xlab("step") + ylab("Shoal Area") +  # axis labels
+  ggtitle("n = 50") +  # chart title text, left justified
+  theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold'))  # label text
+
+area100 <- ggplot() + 
+  theme_classic() + 
+  geom_line(data=num_100, aes(x=step, y=area), colour="#6F115D", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
+  xlab("step") + ylab("Shoal Area") +  # axis labels
+  ggtitle("n = 100") +  # chart title text, left justified
+  theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold'))  # label text
+
+area200 <- ggplot() + 
+  theme_classic() + 
+  geom_line(data=num_200, aes(x=step, y=area), colour="#6F115D", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 10, color = "#1B4460"),  # axis text size & color
+        axis.text.x = element_text(size = 10, color = "#1B4460")) + 
+  theme(axis.line = element_line(color="#1B4460", size = 1)) +
+  xlab("step") + ylab("Shoal Area") +  # axis labels
+  ggtitle("n = 200") +  # chart title text, left justified
+  theme(plot.title = element_text(size = 12, face = 'bold')) +  # title formatting
+  theme(text = element_text(colour = '#1B4460', size = 12, face = 'bold'))  # label text
+
+# Call multiplot function - located in seperate file. Orders plot column by column
+multiplot(area50, area100, area200, cols=1)
+
