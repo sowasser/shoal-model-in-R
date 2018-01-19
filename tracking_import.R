0730ff23f7d10de635b75feef8a01df8db4bd1eb
Sophia Wassermann <so.wassermann@gmail.com>
2018-01-19 15:20:51 +0000

    Working on integrating video data...now how do I write a for loop again?

--- tracking_import.R
+++ tracking_import.R
@@ -2,7 +2,6 @@
 # The program exports a .csv file with x,y position for each individual and the
 # x,y velocity for each. 
 
-setwd("~/Desktop/Local/Mackerel/Mackerel_Data")
 track <- read.csv("tracking_fast_trial_17Nov.csv")
 
 track <- track[c(1, 4:19)]  # Remove empty columns from mistakes while digitizing
@@ -12,3 +11,9 @@ colnames(track) <- c("time", "x1", "y1", "x1_v", "y1_v",
                      "x2", "y2", "x2_v", "y2_v", 
                      "x3", "y3", "x3_v", "y3_v", 
                      "x4", "y4", "x4_v", "y4_v")
+
+pos <- track[c(1, 2, 3, 6, 7, 10, 11, 14, 15)]  # extract position data
+velocity <- track[c(1, 4, 5, 8, 9, 12, 13)]  # extract velocities
+
+# calculate distance between individuals
+
