284128a77a56f3cbbb34052b7108ae094b2b843e
Sophia Wassermann <so.wassermann@gmail.com>
2018-01-19 15:53:36 +0000

    First attempt to get distances between points

--- tracking_import.R
+++ tracking_import.R
@@ -2,6 +2,8 @@
 # The program exports a .csv file with x,y position for each individual and the
 # x,y velocity for each. 
 
+library(foreach)
+
 track <- read.csv("tracking_fast_trial_17Nov.csv")
 
 track <- track[c(1, 4:19)]  # Remove empty columns from mistakes while digitizing
@@ -15,5 +17,9 @@ colnames(track) <- c("time", "x1", "y1", "x1_v", "y1_v",
 pos <- track[c(1, 2, 3, 6, 7, 10, 11, 14, 15)]  # extract position data
 velocity <- track[c(1, 4, 5, 8, 9, 12, 13)]  # extract velocities
 
-# calculate distance between individuals
+# calculate distance between individuals.
+# working from: https://stackoverflow.com/questions/24746892/how-to-calculate-euclidian-distance-between-two-points-defined-by-matrix-contain
+dist <- function(p1, p2) sqrt(sum((p1 - p2) ^ 2))
+
+foreach(i = 1:nrow(p1), .combine = c) %do% dist(p1[i,],p2[i,])
 
