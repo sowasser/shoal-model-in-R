3434805dd6cb78d2263d3015d0d0b16a7b1624d2
Sophia Wassermann <so.wassermann@gmail.com>
2018-01-22 14:02:52 +0000

    Separated position data into individual dataframes.

--- tracking_import.R
+++ tracking_import.R
@@ -17,8 +17,15 @@ colnames(track) <- c("time", "x1", "y1", "x1_v", "y1_v",
 pos <- track[c(1, 2, 3, 6, 7, 10, 11, 14, 15)]  # extract position data
 velocity <- track[c(1, 4, 5, 8, 9, 12, 13)]  # extract velocities
 
-# calculate distance between individuals.
+# calculate Euclidean distance between individuals.
 # working from: https://stackoverflow.com/questions/24746892/how-to-calculate-euclidian-distance-between-two-points-defined-by-matrix-contain
+
+# separate fish into individual dataframes
+pos1 <- pos[c(2, 3)]
+pos2 <- pos[c(4, 5)]
+pos3 <- pos[c(6, 7)]
+pos4 <- pos[c(8, 9)]
+
 dist <- function(p1, p2) sqrt(sum((p1 - p2) ^ 2))
 
 foreach(i = 1:nrow(p1), .combine = c) %do% dist(p1[i,],p2[i,])
