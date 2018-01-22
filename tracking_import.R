9d7274a6c65019a376e3dcf29440d745ad08be51
Sophia Wassermann <so.wassermann@gmail.com>
2018-01-22 17:19:02 +0000

    Euclidean distances between each fish calculated

--- tracking_import.R
+++ tracking_import.R
@@ -18,7 +18,7 @@ pos <- track[c(1, 2, 3, 6, 7, 10, 11, 14, 15)]  # extract position data
 velocity <- track[c(1, 4, 5, 8, 9, 12, 13)]  # extract velocities
 
 # calculate Euclidean distance between individuals.
-# working from: https://stackoverflow.com/questions/24746892/how-to-calculate-euclidian-distance-between-two-points-defined-by-matrix-contain
+# There has GOT to be a better way to do this.
 
 # separate fish into individual dataframes
 pos1 <- pos[c(2, 3)]
@@ -26,7 +26,16 @@ pos2 <- pos[c(4, 5)]
 pos3 <- pos[c(6, 7)]
 pos4 <- pos[c(8, 9)]
 
-dist <- function(p1, p2) sqrt(sum((p1 - p2) ^ 2))
+# working from: https://stackoverflow.com/questions/24746892/how-to-calculate-euclidian-distance-between-two-points-defined-by-matrix-contain
+dist <- function(p1, p2) sqrt(sum((p1 - p2) ^ 2))  # function for Euclidean dist
 
-foreach(i = 1:nrow(p1), .combine = c) %do% dist(p1[i,],p2[i,])
+# Distance between each different combination of fish. Blerg.
+dist1_2 = foreach(i = 1:nrow(pos1), .combine = c) %do% dist(pos1[i,],pos2[i,])
+dist1_3 = foreach(i = 1:nrow(pos1), .combine = c) %do% dist(pos1[i,],pos3[i,])
+dist1_4 = foreach(i = 1:nrow(pos1), .combine = c) %do% dist(pos1[i,],pos4[i,])
+dist2_3 = foreach(i = 1:nrow(pos2), .combine = c) %do% dist(pos2[i,],pos3[i,])
+dist2_4 = foreach(i = 1:nrow(pos2), .combine = c) %do% dist(pos2[i,],pos4[i,])
+dist3_4 = foreach(i = 1:nrow(pos3), .combine = c) %do% dist(pos3[i,],pos4[i,])
 
+# Combine into one dataframe. Maybe not necessary?
+distances <- cbind(pos[1], dist1_2, dist1_3, dist1_4, dist2_3, dist2_4, dist3_4)
