5ffce55f1301ac5fa5d9b1bc0fcda12b933d631f
Sophia Wassermann <so.wassermann@gmail.com>
2018-12-10 15:56:18 +0000

    Axis ticks now match color of line, which is colored for IEA conference
    Also changed around the order of the elements in the style and updated the formatting & comments

--- tracking_graphs.R
+++ tracking_graphs.R
@@ -28,57 +28,65 @@ colnames(polar) <- c("step", "polar")
 
 cent_dist_graph <- ggplot() + 
   theme_classic() + 
-  geom_line(data=cent_dist, aes(x=step, y=dist), colour="blue", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "black"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "black")) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  geom_line(data=cent_dist, aes(x=step, y=dist), colour="#D97219", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "#80807F"),  # axis text size & color
+        axis.text.x = element_text(size = 14, color = "#80807F")) + 
   xlab("step") + ylab("Mean Distance from Centroid (mm)") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 16, face = 'bold')) + # label text
-  theme(panel.background = element_rect(fill = "transparent"),
+  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold")) + # label text
+  theme(axis.line = element_line(color="#80807F", size = 1),  # axis line & tick color
+        axis.ticks = element_line(color="#80807F")) +
+  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
         plot.background = element_rect(fill = "transparent", size = 0))
 cent_dist_graph
-ggsave(cent_dist_graph, filename = "step1_cent_dist.png", width=8, height=5.5, units="in", dpi = 300, bg = "transparent")
+ggsave(cent_dist_graph, filename = "step1_cent_dist.png", width = 8, height = 5.5, units = "in", 
+       dpi = 300, bg = "transparent")
 
 
 nnd_graph <- ggplot() + 
   theme_classic() + 
-  geom_line(data=nnd, aes(x=step, y=nnd), colour="blue", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "black"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "black")) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  geom_line(data=nnd, aes(x=step, y=nnd), colour="#D97219", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "#80807F"),  # axis text size & color
+        axis.text.x = element_text(size = 14, color = "#80807F")) + 
   xlab("step") + ylab("Mean Nearest Neighbour Distance (mm)") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 16, face = 'bold')) + # label text
-  theme(panel.background = element_rect(fill = "transparent"),
+  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold")) + # label text
+  theme(axis.line = element_line(color="#80807F", size = 1), # axis line & tick color
+        axis.ticks = element_line(color="#80807F")) +
+  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
         plot.background = element_rect(fill = "transparent", size = 0))
 nnd_graph
-ggsave(nnd_graph, filename = "step1_nnd.png", width=8, height=5.5, units="in", dpi = 300, bg = "transparent")
+ggsave(nnd_graph, filename = "step1_nnd.png", width = 8, height = 5.5, units = "in", 
+       dpi = 300, bg = "transparent")
 
 area_graph <- ggplot() + 
   theme_classic() + 
-  geom_line(data=area, aes(x=step, y=area), colour="blue", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "black"),  # axis text size & color
+  geom_line(data=area, aes(x=step, y=area), colour="#D97219", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "#80807F"),  # axis text size & color
         axis.text.x = element_text(size = 14, color = "black")) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
   xlab("step") + ylab("Shoal Area (mm2)") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 16, face = 'bold'))  + # label text
-  theme(panel.background = element_rect(fill = "transparent"),
+  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold"))  + # label text
+  theme(axis.line = element_line(color="#80807F", size = 1),  # axis line & tick color
+        axis.ticks = element_line(color="#80807F")) +
+  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
         plot.background = element_rect(fill = "transparent", size = 0))
 area_graph
-ggsave(area_graph, filename = "step1_area.png", width=8, height=5.5, units="in", dpi = 300, bg = "transparent")
+ggsave(area_graph, filename = "step1_area.png", width = 8, height = 5.5, units = "in", 
+       dpi = 300, bg = "transparent")
 
 polar_graph <- ggplot() + 
   theme_classic() + 
-  geom_line(data=polar, aes(x=step, y=polar), colour="blue", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "black"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "black")) + 
-  theme(axis.line = element_line(color="black", size = 1)) +
+  geom_line(data=polar, aes(x=step, y=polar), colour="#D97219", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "#80807F"),  # axis text size & color
+        axis.text.x = element_text(size = 14, color = "#80807F")) + 
   xlab("step") + ylab("Polarisation") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = 'bold')) +  # title formatting
-  theme(text = element_text(colour = 'black', size = 16, face = 'bold'))  + # label text
-  theme(panel.background = element_rect(fill = "transparent"),
+  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold"))  + # label text
+  theme(axis.line = element_line(color="#80807F", size = 1),  # axis line & tick color
+        axis.ticks = element_line(color="#80807F")) +
+  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
         plot.background = element_rect(fill = "transparent", size = 0))
 polar_graph
-ggsave(polar_graph, filename = "step1_polar.png", width=8, height=5.5, units="in", dpi = 300, bg = "transparent")
+ggsave(polar_graph, filename = "step1_polar.png", width = 8, height = 5.5, units = "in", 
+       dpi = 300, bg = "transparent")
