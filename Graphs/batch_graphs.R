6e70e14595ccc266c223eb5f65215bae102be159
Sophia Wassermann <so.wassermann@gmail.com>
2019-05-30 14:52:33 +0100

    Changed over script to follow new ways of calculating means from model

--- Graphs/batch_graphs.R
+++ Graphs/batch_graphs.R
@@ -1,85 +1,78 @@
 # Script for building graphs for multiple runs of the shoaling model, currently
-# over 100 steps and 10 runs.
+# over 100 steps and 100 runs.
+
 # Current data outputs from the model are:
 # 1. Nearest Neighbour Distance
 # 2. Polarization (median absolute deviation in heading)
 # 3. Shoal Area (convex hull)
 # 4. Mean distance from centroid
 
-setwd("~/Desktop/Local/Mackerel/Mackerel Data")
+# The means are calculated two ways:
+# 1. Mean of all runs, calculated for every step of the model - mean runs.
+# 2. Mean of all steps, calculated for every run of the model - mean steps.
 
 library(ggplot2)
+source("~/Desktop/Local/Mackerel/shoal-model-in-R/multiplot.R")
 
-  
-polar_mean <- read.csv("polar_mean.csv")
-colnames(polar_mean) <- c("step", "polar")
+# path <- "~/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data/"  # for laptop
+path <- "~/Desktop/Local/Mackerel/Mackerel Data/"  # for desktop
 
-nnd_mean <- read.csv("nnd_mean.csv")
-colnames(nnd_mean) <- c("step", "nnd")
-
-area_mean <- read.csv("area_mean.csv")
-colnames(area_mean) <- c("step", "area")
-
-cent_mean <- read.csv("cent_mean.csv")
-colnames(cent_mean) <- c("step", "cent")
+run_means <- read.csv(paste0(path,"batch_means_runs.csv"))
+step_means <- read.csv(paste0(path,"batch_means_steps.csv"))
 
 
-polar_mean_graph <- ggplot() + 
+polar_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=polar_mean, aes(x=step, y=polar), colour="#DA7318", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "#737373"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "#737373")) + 
+  geom_line(data=run_means, aes(x=X, y=polar), colour="#C830CC", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+        axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Polarization") +  # axis labels
   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold"))  + # label text
-  theme(axis.line = element_line(color="#737373", size = 1),  # axis line & tick color
-        axis.ticks = element_line(color="#737373")) +
-  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
-        plot.background = element_rect(fill = "transparent", size = 0))
-ggsave(polar_mean_graph, filename = "polar_mean.png", width = 17.09, height = 11.76, units = "cm", 
-       dpi = 300, bg = "transparent")
-
-nnd_mean_graph <- ggplot() + 
+  theme(text = element_text(colour = "white", size = 16, face = "bold"))  + # label text
+  theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
+        axis.ticks = element_line(color="white")) +
+  theme(panel.background = element_rect(fill = "#27292F"),  # transparent background & no outline
+        plot.background = element_rect(fill = "#27292F", size = 0))
+nnd_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=nnd_mean, aes(x=step, y=nnd), colour="#DA7318", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "#737373"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "#737373")) + 
+  geom_line(data=run_means, aes(x=X, y=nnd), colour="#C830CC", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+        axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Nearest Neighbour Distance") +  # axis labels
   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold")) + # label text
-  theme(axis.line = element_line(color="#737373", size = 1), # axis line & tick color
-        axis.ticks = element_line(color="#737373")) +
-  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
-        plot.background = element_rect(fill = "transparent", size = 0))
-ggsave(nnd_mean_graph, filename = "nnd_mean.png", width = 17.09, height = 11.76, units = "cm", 
-       dpi = 300, bg = "transparent")
-
-area_mean_graph <- ggplot() + 
+  theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
+  theme(axis.line = element_line(color="white", size = 1), # axis line & tick color
+        axis.ticks = element_line(color="white")) +
+  theme(panel.background = element_rect(fill = "#27292F"),  # transparent background & no outline
+        plot.background = element_rect(fill = "#27292F", size = 0))
+area_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=area_mean, aes(x=step, y=area), colour="#DA7318", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "#737373"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "#737373")) + 
+  geom_line(data=run_means, aes(x=X, y=area), colour="#C830CC", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+        axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Shoal Area") +  # axis labels
   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold"))  + # label text
-  theme(axis.line = element_line(color="#737373", size = 1),  # axis line & tick color
-        axis.ticks = element_line(color="#737373")) +
-  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
-        plot.background = element_rect(fill = "transparent", size = 0))
-ggsave(area_mean_graph, filename = "area_mean.png", width = 17.09, height = 11.76, units = "cm", 
-       dpi = 300, bg = "transparent")
-
-cent_mean_graph <- ggplot() + 
+  theme(text = element_text(colour = "white", size = 16, face = "bold"))  + # label text
+  theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
+        axis.ticks = element_line(color="white")) +
+  theme(panel.background = element_rect(fill = "#27292F"),  # transparent background & no outline
+        plot.background = element_rect(fill = "#27292F", size = 0))
+cent_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=cent_mean, aes(x=step, y=cent), colour="#DA7318", size = 1) +  # line
-  theme(axis.text.y = element_text(size = 14, color = "#737373"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "#737373")) + 
+  geom_line(data=run_means, aes(x=X, y=centroid), colour="#C830CC", size = 1) +  # line
+  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+        axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Distance from Centroid") +  # axis labels
   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "#A6B7C8", size = 16, face = "bold")) + # label text
-  theme(axis.line = element_line(color="#737373", size = 1),  # axis line & tick color
-        axis.ticks = element_line(color="#737373")) +
-  theme(panel.background = element_rect(fill = "transparent"),  # transparent background & no outline
-        plot.background = element_rect(fill = "transparent", size = 0))
+  theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
+  theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
+        axis.ticks = element_line(color="white")) +
+  theme(panel.background = element_rect(fill = "#27292F"),  # transparent background & no outline
+        plot.background = element_rect(fill = "#27292F", size = 0))
 ggsave(cent_mean_graph, filename = "cent_mean.png", width = 17.09, height = 11.76, units = "cm", 
        dpi = 300, bg = "transparent")
+
+# Call multiplot function for mean graphs
+png("run_means.png", width = 11, height = 8, units = 'in', res = 300)
+multiplot(polar_runs, nnd_runs, area_runs, cent_runs, cols=2)
+dev.off()
