a1cf6734543f0395160eabf0437952b450297488
Sophia Wassermann <so@sowasser.com>
2019-06-09 19:28:43 +0100

    Added code for boxplots. For better plots, need to code categories

--- ABC/abc.R
+++ ABC/abc.R
@@ -15,23 +15,21 @@
 library(abc)
 library(ggplot2)
 
-# path <- "~/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data/"  # for laptop
-path <- "~/Desktop/Local/Mackerel/Mackerel Data/"  # for desktop
+path <- "~/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data/"  # for laptop
+# path <- "~/Desktop/Local/Mackerel/Mackerel Data/"  # for desktop
 
 
 # Read in data & calculate means ----------------------------------------------
 
-# Mean of all runs, calculated for every step of the model
-run_means <- read.csv(paste0(path,"batch_means_runs.csv"))
-# Mean of all steps, calculated for every run of the model
-step_means <- read.csv(paste0(path,"batch_means_steps.csv"))
+# Mean of all runs, calculated for every step of the model & changes in a variable
+model_means <- read.csv(paste0(path,"means_var-speed.csv"))
 
 # Calculate overall means for each data collector
-# overall_means <- data.frame(colMeans(run_means))
-polar_model_mean <- mean(run_means$polar)
-nnd_model_mean <- mean(run_means$nnd)
-area_model_mean <- mean(run_means$area)
-cent_model_mean <- mean(run_means$centroid)
+# overall_means <- data.frame(colMeans(model_means))
+polar_model_mean <- mean(model_means$polar)
+nnd_model_mean <- mean(model_means$nnd)
+area_model_mean <- mean(model_means$area)
+cent_model_mean <- mean(model_means$centroid)
 
 # Data from video tracking
 tracking <- read.csv(paste0(path, "stepwise_data.csv"))
@@ -45,8 +43,25 @@ cent_track_mean <- mean(tracking$centroid)
 
 # Check summary statistics ----------------------------------------------------
 # boxplots
+polar_plot <- ggplot(data = model_means, aes(x=var, y=polar, group=var)) + 
+  theme_classic() +
+  geom_boxplot()
+polar_plot
 
+nnd_plot <- ggplot(data = model_means, aes(x=var, y=nnd, group=var)) + 
+  theme_classic() +
+  geom_boxplot()
+nnd_plot
 
+area_plot <- ggplot(data = model_means, aes(x=var, y=area, group=var)) + 
+  theme_classic() +
+  geom_boxplot()
+area_plot
+
+cent_plot <- ggplot(data = model_means, aes(x=var, y=centroid, group=var)) + 
+  theme_classic() +
+  geom_boxplot()
+cent_plot
 
 # Run ABC ---------------------------------------------------------------------
 # import matrix of simulated summary statistics - from model
