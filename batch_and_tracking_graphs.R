0893e93fe289515c4598d2d51eefd4bffc03f3b2
Sophia Wassermann <so.wassermann@gmail.com>
2019-09-04 14:51:50 +0100

    Now just looking at means for each step

--- batch_and_tracking_graphs.R
+++ batch_and_tracking_graphs.R
@@ -17,15 +17,14 @@ multi_path <- "~/Desktop/Local/Mackerel/shoal-model-in-R/"  # Path for multiplot
 source(paste0(multi_path,"multiplot.R"))
 
 # Read in data ----------------------------------------------------------------
-run_means <- read.csv(paste0(path,"batch_means_runs.csv"))
+step_means <- read.csv(paste0(path,"step_means.csv"))
 tracking <- read.csv(paste0(path, "stepwise_data.csv"))
-model_means <- read.csv(paste0(path,"means_var-speed.csv"))
 
 # Run means for every step of the models --------------------------------------
 
 polar_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=run_means, aes(x=X, y=polar), colour="#C830CC", size = 1) +  # line
+  geom_line(data=step_means, aes(x=X, y=polar), colour="#C830CC", size = 1) +  # line
   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
         axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Polarization") +  # axis labels
@@ -38,7 +37,7 @@ polar_runs <- ggplot() +
 
 nnd_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=run_means, aes(x=X, y=nnd), colour="#C830CC", size = 1) +  # line
+  geom_line(data=step_means, aes(x=X, y=nnd), colour="#C830CC", size = 1) +  # line
   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
         axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Nearest Neighbour Distance") +  # axis labels
@@ -51,7 +50,7 @@ nnd_runs <- ggplot() +
 
 area_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=run_means, aes(x=X, y=area), colour="#C830CC", size = 1) +  # line
+  geom_line(data=step_means, aes(x=X, y=area), colour="#C830CC", size = 1) +  # line
   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
         axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Shoal Area") +  # axis labels
@@ -64,7 +63,7 @@ area_runs <- ggplot() +
 
 cent_runs <- ggplot() + 
   theme_classic() + 
-  geom_line(data=run_means, aes(x=X, y=centroid), colour="#C830CC", size = 1) +  # line
+  geom_line(data=step_means, aes(x=X, y=centroid), colour="#C830CC", size = 1) +  # line
   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
         axis.text.x = element_text(size = 14, color = "white")) + 
   xlab("step") + ylab("Mean Distance from Centroid") +  # axis labels
@@ -132,65 +131,64 @@ track_cent <- ggplot() +
 
 
 # Boxplots for mean data collectors over all steps & runs ---------------------
-polar_box <- ggplot() + 
-  theme_classic() +
-  geom_boxplot(data = model_means, aes(x=var, y=polar, group=var), # boxplot
-               colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
-  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "white")) + 
-  xlab("speed") + ylab("Mean Polarization") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
-  theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
-        axis.ticks = element_line(color="white")) +
-  theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
-        plot.background = element_rect(fill = "#27282E", size = 0))
-
-nnd_box <- ggplot() + 
-  theme_classic() +
-  geom_boxplot(data = model_means, aes(x=var, y=nnd, group=var), # boxplot
-               colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
-  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "white")) + 
-  xlab("speed") + ylab("Mean Nearest Neighbour Distance") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
-  theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
-        axis.ticks = element_line(color="white")) +
-  theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
-        plot.background = element_rect(fill = "#27282E", size = 0))
-
-area_box <- ggplot() + 
-  theme_classic() +
-  geom_boxplot(data = model_means, aes(x=var, y=area, group=var), # boxplot
-               colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
-  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "white")) + 
-  xlab("speed") + ylab("Mean Shoal Area") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
-  theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
-        axis.ticks = element_line(color="white")) +
-  theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
-        plot.background = element_rect(fill = "#27282E", size = 0))
-
-cent_box <- ggplot() + 
-  theme_classic() +
-  geom_boxplot(data = model_means, aes(x=var, y=centroid, group=var), # boxplot
-               colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
-  theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
-        axis.text.x = element_text(size = 14, color = "white")) + 
-  xlab("speed") + ylab("Mean Distance from Centroid") +  # axis labels
-  theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
-  theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
-  theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
-        axis.ticks = element_line(color="white")) +
-  theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
-        plot.background = element_rect(fill = "#27282E", size = 0))
+# polar_box <- ggplot() + 
+#   theme_classic() +
+#   geom_boxplot(data = model_means, aes(x=var, y=polar, group=var), # boxplot
+#                colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
+#   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+#         axis.text.x = element_text(size = 14, color = "white")) + 
+#   xlab("speed") + ylab("Mean Polarization") +  # axis labels
+#   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+#   theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
+#   theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
+#         axis.ticks = element_line(color="white")) +
+#   theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
+#         plot.background = element_rect(fill = "#27282E", size = 0))
+# 
+# nnd_box <- ggplot() + 
+#   theme_classic() +
+#   geom_boxplot(data = model_means, aes(x=var, y=nnd, group=var), # boxplot
+#                colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
+#   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+#         axis.text.x = element_text(size = 14, color = "white")) + 
+#   xlab("speed") + ylab("Mean Nearest Neighbour Distance") +  # axis labels
+#   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+#   theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
+#   theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
+#         axis.ticks = element_line(color="white")) +
+#   theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
+#         plot.background = element_rect(fill = "#27282E", size = 0))
+# 
+# area_box <- ggplot() + 
+#   theme_classic() +
+#   geom_boxplot(data = model_means, aes(x=var, y=area, group=var), # boxplot
+#                colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
+#   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+#         axis.text.x = element_text(size = 14, color = "white")) + 
+#   xlab("speed") + ylab("Mean Shoal Area") +  # axis labels
+#   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+#   theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
+#   theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
+#         axis.ticks = element_line(color="white")) +
+#   theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
+#         plot.background = element_rect(fill = "#27282E", size = 0))
+# 
+# cent_box <- ggplot() + 
+#   theme_classic() +
+#   geom_boxplot(data = model_means, aes(x=var, y=centroid, group=var), # boxplot
+#                colour="white",  fill="#C830CC", width=4) +  # line & box fill colors; box width
+#   theme(axis.text.y = element_text(size = 14, color = "white"),  # axis text size & color
+#         axis.text.x = element_text(size = 14, color = "white")) + 
+#   xlab("speed") + ylab("Mean Distance from Centroid") +  # axis labels
+#   theme(plot.title = element_text(size = 16, face = "bold")) +  # title formatting
+#   theme(text = element_text(colour = "white", size = 16, face = "bold")) + # label text
+#   theme(axis.line = element_line(color="white", size = 1),  # axis line & tick color
+#         axis.ticks = element_line(color="white")) +
+#   theme(panel.background = element_rect(fill = "#27282E"),  # plot & background colors, no outline
+#         plot.background = element_rect(fill = "#27282E", size = 0))
 
 # Call multiplot function to combine graphs -----------------------------------
-png("~/Desktop/means_with_tracking.png", width = 20, height = 18, units = 'in', res = 300)
-multiplot(polar_box, nnd_box, area_box, cent_box, polar_runs,  # multiplot fills by column
-          nnd_runs, area_runs, cent_runs,
-          track_polar, track_nnd, track_area, track_cent, cols=3)
+png("~/Desktop/means_with_tracking.png", width = 16, height = 14, units = 'in', res = 300)
+multiplot(polar_runs, nnd_runs, cent_runs, # multiplot fills by column
+          track_polar, track_nnd, track_cent, cols=2)
 dev.off()
