dca2b2b65dd63ee92afac13aef3785e6a6a43a5e
Sophia Wassermann <so.wassermann@gmail.com>
2019-12-11 16:28:19 +0000

    Figured out multiple plots in ggplot, but need to figure out the order

--- ABC/abc.R
+++ ABC/abc.R
@@ -14,6 +14,7 @@
 
 library(abc)
 library(ggplot2)
+library(gridExtra)
 
 # path <- "~/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data/"  # for laptop
 path <- "~/Desktop/Local/Mackerel/Mackerel Data/"  # for desktop
@@ -35,9 +36,41 @@ tracking <- read.csv(paste0(path, "stepwise_data.csv"))
 # Check summary statistics ----------------------------------------------------
 # set "x" and "group" to the parameter you're looking at
 # set "y" to the summary statistic you're looking at
-# Todo: change this to something that works better
-ggplot(data = model, aes(x=speed, y=polar)) + 
+# TODO: change this to something that works better - look up facets!
+
+require(gridExtra)
+speed_polar <- ggplot(data = model, aes(x=speed, y=polar)) + 
   theme_classic() + geom_point()
+speed_nnd <- ggplot(data = model, aes(x=speed, y=nnd)) + 
+  theme_classic() + geom_point()
+speed_area <- ggplot(data = model, aes(x=speed, y=area)) + 
+  theme_classic() + geom_point()
+speed_cent <- ggplot(data = model, aes(x=speed, y=centroid)) + 
+  theme_classic() + geom_point()
+
+vision_polar <- ggplot(data = model, aes(x=vision, y=polar)) + 
+  theme_classic() + geom_point()
+vision_nnd <- ggplot(data = model, aes(x=vision, y=nnd)) + 
+  theme_classic() + geom_point()
+vision_area <- ggplot(data = model, aes(x=vision, y=area)) + 
+  theme_classic() + geom_point()
+vision_cent <- ggplot(data = model, aes(x=vision, y=centroid)) + 
+  theme_classic() + geom_point()
+
+sep_polar <- ggplot(data = model, aes(x=separation, y=polar)) + 
+  theme_classic() + geom_point()
+sep_nnd <- ggplot(data = model, aes(x=separation, y=nnd)) + 
+  theme_classic() + geom_point()
+sep_area <- ggplot(data = model, aes(x=separation, y=area)) + 
+  theme_classic() + geom_point()
+sep_cent <- ggplot(data = model, aes(x=separation, y=centroid)) + 
+  theme_classic() + geom_point()
+
+# TODO: deterime fill order!
+grid.arrange(speed_polar, speed_nnd, speed_area, speed_cent,
+             vision_polar, vision_nnd, vision_area, vision_cent,
+             sep_polar, sep_nnd, sep_area, sep_cent, ncol=3)
+
 
 pairs(model[,], pch = 19, lower.panel = NULL)
 
