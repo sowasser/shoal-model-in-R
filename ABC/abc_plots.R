40ec38389f55008498da1031a6a0c4e4df03be1a
Sophia Wassermann <hi@sowasser.com>
2020-04-16 10:58:27 +0100

    Reorganized data into form that will work with ggplot boxplot

--- ABC/abc_plots.R
+++ ABC/abc_plots.R
@@ -54,8 +54,38 @@ grid.arrange(speed_polar, vision_polar, sep_polar, track_polar,
 
 
 # Prior/posterior distributions from abc --------------------------------------
-# Plan is boxplots (either grouped or multiple plots for the prior & posterior
-# distributions of each statistic)
+# Data needs to be transformed to be one vector of values labeled with which
+# statistic it is and which distribution it's from
+
+# Create labels for statistics
+sd <- rep("speed", length(model_params$speed))
+vs <- rep("vision", length(model_params$vision))
+sp <- rep("separation", length(model_params$sep))
+
+# Combine values with their statistic label in one column
+prior_sd <- cbind(model_params$speed, sd)
+prior_vs <- cbind(model_params$vision, vs)
+prior_sp <- cbind(model_params$sep, sp)
+priors <- as.data.frame(rbind(prior_sd, prior_vs, prior_sp))
+
+# Combine values and statistics label in one column with distribution label
+cat_priors <- rep("prior", length(priors$sd))
+priors <- cbind(priors, cat_priors)
+colnames(priors) <- c("value", "statistic", "distribution")
+
+# Combine values with their statistic label in one column
+post_all <- as.data.frame(shoaling.abc$unadj.values)
+post_sd <- cbind(post_all$speed, sd)
+post_vs <- cbind(post_all$vision, vs)
+post_sp <- cbind(post_all$sep, sp)
+posts <- as.data.frame(rbind(post_sd, post_vs, post_sp))
+
+# Combine values and statistics label in one column with distribution label
+cat_posts <- rep("posterior", length(posts$sd))
+posts <- cbind(posts, cat_posts)
+colnames(posts) <- c("value", "statistic", "distribution")
+
+dists <- rbind(priors, posts)  # Combine everything into one dataframe
 
 priors <- model_params
 posteriors <- shoaling.abc$unadj.values
