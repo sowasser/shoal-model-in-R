79dd4b71e66eaaefc08a346d8a4f160bdc6fe449
Sophia Wassermann <hi@sowasser.com>
2020-05-05 13:13:06 +0100

    Updated to work with the data imported through the  main abc script

--- ABC/abc_plots.R
+++ ABC/abc_plots.R
@@ -4,14 +4,21 @@ library(ggplot2)
 library(gridExtra)
 
 # Tracking Graphs -------------------------------------------------------------
+# New tracking dataframe just for graphs
+track_graphs <- tracking
+
+# Add "step" column
+track_graphs$step <- 1:nrow(track_graphs)
+
+
 require(gridExtra)
-track_cent <- ggplot(data = tracking, aes(x=step, y=centroid)) +
+track_cent <- ggplot(data = track_graphs, aes(x=step, y=cent)) +
   theme_classic() + geom_point() + geom_line()
-track_nnd <- ggplot(data = tracking, aes(x=step, y=nnd)) +
+track_nnd <- ggplot(data = track_graphs, aes(x=step, y=nnd)) +
   theme_classic() + geom_point() + geom_line()
-track_area <- ggplot(data = tracking, aes(x=step, y=area)) +
+track_area <- ggplot(data = track_graphs, aes(x=step, y=area)) +
   theme_classic() + geom_point() + geom_line()
-track_polar <- ggplot(data = tracking, aes(x=step, y=polar)) +
+track_polar <- ggplot(data = track_graphs, aes(x=step, y=polar)) +
   theme_classic() + geom_point() + geom_line()
 
 # Model Graphs ----------------------------------------------------------------
@@ -19,35 +26,35 @@ require(gridExtra)
 # set "x" and "group" to the parameter you're looking at
 # set "y" to the summary statistic you're looking at
 # TODO: change this to something that works better - maybe facets?
-speed_polar <- ggplot(data = model, aes(x=speed, y=polar)) + 
+speed_polar <- ggplot(data = model, aes(x=speed, y=polar_mean)) + 
   theme_classic() + geom_point()
-speed_nnd <- ggplot(data = model, aes(x=speed, y=nnd)) + 
+speed_nnd <- ggplot(data = model, aes(x=speed, y=nnd_mean)) + 
   theme_classic() + geom_point()
-speed_area <- ggplot(data = model, aes(x=speed, y=area)) + 
+speed_area <- ggplot(data = model, aes(x=speed, y=area_mean)) + 
   theme_classic() + geom_point()
-speed_cent <- ggplot(data = model, aes(x=speed, y=centroid)) + 
-  theme_classic() + geom_point()
-
-vision_polar <- ggplot(data = model, aes(x=vision, y=polar)) + 
-  theme_classic() + geom_point()
-vision_nnd <- ggplot(data = model, aes(x=vision, y=nnd)) + 
-  theme_classic() + geom_point()
-vision_area <- ggplot(data = model, aes(x=vision, y=area)) + 
-  theme_classic() + geom_point()
-vision_cent <- ggplot(data = model, aes(x=vision, y=centroid)) + 
+speed_cent <- ggplot(data = model, aes(x=speed, y=cent_mean)) + 
   theme_classic() + geom_point()
 
-sep_polar <- ggplot(data = model, aes(x=separation, y=polar)) + 
+vision_polar <- ggplot(data = model, aes(x=vision, y=polar_mean)) + 
   theme_classic() + geom_point()
-sep_nnd <- ggplot(data = model, aes(x=separation, y=nnd)) + 
+vision_nnd <- ggplot(data = model, aes(x=vision, y=nnd_mean)) + 
   theme_classic() + geom_point()
-sep_area <- ggplot(data = model, aes(x=separation, y=area)) + 
+vision_area <- ggplot(data = model, aes(x=vision, y=area_mean)) + 
   theme_classic() + geom_point()
-sep_cent <- ggplot(data = model, aes(x=separation, y=centroid)) + 
+vision_cent <- ggplot(data = model, aes(x=vision, y=cent_mean)) + 
+  theme_classic() + geom_point()
+
+sep_polar <- ggplot(data = model, aes(x=sep, y=polar_mean)) + 
+  theme_classic() + geom_point()
+sep_nnd <- ggplot(data = model, aes(x=sep, y=nnd_mean)) + 
+  theme_classic() + geom_point()
+sep_area <- ggplot(data = model, aes(x=sep, y=area_mean)) + 
+  theme_classic() + geom_point()
+sep_cent <- ggplot(data = model, aes(x=sep, y=cent_mean)) + 
   theme_classic() + geom_point()
 
 # Create multiple graphs in page, row by row ----------------------------------
-grid.arrange(speed_polar, vision_polar, sep_polar, track_polar,
+prior_data <- grid.arrange(speed_polar, vision_polar, sep_polar, track_polar,
              speed_nnd, vision_nnd, sep_nnd, track_nnd,
              speed_area, vision_area, sep_area, track_area,
              speed_cent, vision_cent, sep_cent, track_cent, ncol=4)
