2ebf0d370ce3a1c86351b6bd1926cca27f93833f
Sophia Wassermann <hi@sowasser.com>
2020-05-20 14:52:15 +0100

    Slightly condensed the code for the data transformations for these plots

--- ABC/abc_plots.R
+++ ABC/abc_plots.R
@@ -9,38 +9,28 @@ library(gridExtra)
 
 
 
-
-
 # Prior/posterior distributions from abc --------------------------------------
 # Data needs to be transformed to be one vector of values labeled with which
-# parameter it is and which distribution it's from
+# parameter it is and which distribution it's from.
 
-# Create labels for parameters
-sd <- rep("speed", length(model_params$speed))
-vs <- rep("vision", length(model_params$vision))
-sp <- rep("separation", length(model_params$sep))
-
-# Combine values with their parameter label in one column
-prior_sd <- cbind(model_params$speed, sd)
-prior_vs <- cbind(model_params$vision, vs)
-prior_sp <- cbind(model_params$sep, sp)
+# For prior distribution:
+# Create array with one column for label, one for the parameter
+prior_sd <- cbind(model_params$speed, rep("speed", length(model_params$speed)))
+prior_vs <- cbind(model_params$vision, rep("vision", length(model_params$vision)))
+prior_sp <- cbind(model_params$sep, rep("separation", length(model_params$sep)))
+# Stack all into one dataframe
 priors <- as.data.frame(rbind(prior_sd, prior_vs, prior_sp))
-
 # Combine values and parameter label in one column with distribution label
-cat_priors <- rep("prior", length(priors$sd))
-priors <- cbind(priors, cat_priors)
+priors <- cbind(priors, rep("prior", length(priors$V1)))
 colnames(priors) <- c("value", "parameter", "distribution")
 
-# Combine values with their statistic label in one column
+# Repeat for posterior distribution
 post_all <- as.data.frame(shoaling.abc$unadj.values)
-post_sd <- cbind(post_all$speed, sd)
-post_vs <- cbind(post_all$vision, vs)
-post_sp <- cbind(post_all$sep, sp)
+post_sd <- cbind(post_all$speed, rep("speed", length(post_all$speed)))
+post_vs <- cbind(post_all$vision, rep("vision", length(post_all$vision)))
+post_sp <- cbind(post_all$sep, rep("separation", length(post_all$sep)))
 posts <- as.data.frame(rbind(post_sd, post_vs, post_sp))
-
-# Combine values and parameter label in one column with distribution label
-cat_posts <- rep("posterior", length(posts$sd))
-posts <- cbind(posts, cat_posts)
+posts <- cbind(posts, rep("posterior", length(posts$V1)))
 colnames(posts) <- c("value", "parameter", "distribution")
 
 dists <- rbind(priors, posts)  # Combine everything into one dataframe
