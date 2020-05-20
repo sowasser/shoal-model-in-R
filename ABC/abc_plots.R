cdcd01267f49ca3d29d8f812d1a4c0df5eea43f0
Sophia Wassermann <hi@sowasser.com>
2020-05-20 15:29:18 +0100

    Added code for plotting the summary statistics for real & modelled data

--- ABC/abc_plots.R
+++ ABC/abc_plots.R
@@ -6,7 +6,58 @@ library(ggplot2)
 library(gridExtra)
 
 # Comparison of summary stats from model vs. real fish ------------------------
+# Data needs to be transformed to be one vector of values labeled with which 
+# data source it's from.
 
+# For tracking data: 
+# Create array with one column for label, one for the statistic
+tc <- cbind(tracking$cent, rep("cent", length(tracking$cent)))
+tn <- cbind(tracking$cent, rep("nnd", length(tracking$nnd)))
+tp <- cbind(tracking$polar, rep("polar", length(tracking$polar)))
+ta <- cbind(tracking$area, rep("area", length(tracking$area)))
+# Stack all into one dataframe
+track_stats <- as.data.frame(rbind(tc, tn, tp, ta))
+# Combine values and statistic label in one column with data source label
+track_stats <- cbind(track_stats, rep("tracking", length(track_stats$V1)))
+colnames(track_stats) <- c("value", "statistic", "data_source")
+
+# Repeat for model data
+mc <- cbind(model$cent_mean, rep("cent", length(model$cent_mean)))
+mn <- cbind(model$nnd_mean, rep("nnd", length(model$nnd_mean)))
+mp <- cbind(model$polar_mean, rep("polar", length(model$polar_mean)))
+ma <- cbind(model$area_mean, rep("area", length(model$area_mean)))
+model_stats <- as.data.frame(rbind(mc, mn, mp, ma))
+model_stats <- cbind(model_stats, rep("model", length(model_stats$V1)))
+colnames(model_stats) <- c("value", "statistic", "data_source")
+
+stats <- rbind(track_stats, model_stats)  # Combine everything in to one dataframe
+
+# Make sure all columns will be recognised appropriately for R/ggplot
+stats$value <- as.numeric(as.character(stats$value))
+stats$statistic <- as.factor(stats$statistic)
+stats$data_source <- as.factor(stats$data_source)
+
+# Text for facet labels
+stat_labs <- c("distance from centroid", 
+               "nearest neighbour distance",
+               "polarization",
+               "shoal area")
+names(stat_labs) <- c("cent", "nnd", "polar", "area")
+
+stat_boxplot <- ggplot(stats, aes(x = statistic, y = value, fill = data_source)) +
+  geom_boxplot() +
+  theme_bw() +
+  xlab(" ") + ylab(" ") +  # axis labels
+  labs(fill = "data source") +  # legend title
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  facet_wrap(~statistic, scale="free", labeller = labeller(statistic = stat_labs))
+
+pdf(paste0("~/Desktop/stat_boxplot_", date, ".pdf"))
+print(stat_boxplot)
+dev.off()
 
 
 # Prior/posterior distributions from abc --------------------------------------
