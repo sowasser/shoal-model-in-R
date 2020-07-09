2a9e0ce2d5ae184d0524054cbc37e4d305e47200
Sophia Wassermann <hi@sowasser.com>
2020-07-09 11:42:59 +0100

    Now using reshape2 package to change the dataframes from wide to long.

--- ABC/abc_plots.R
+++ ABC/abc_plots.R
@@ -1,53 +1,35 @@
 # Code for plots to check:
   # 1. prior & posterior distributions for the abc model (from abc.R).
   # 2. The true vs. estimated values for the parameters following cross-validation
+# Have to run script abc.R before this to import/create variables.
 
 library(ggplot2)
+library(reshape2)
 
 # custom_color <- c("#463682", "#287D8E", "#3CBB76", "#DCE41A")
 custom_color <- c("#404387", "#22A784", "#790251", "#2A788E", "#45015A", "#fDE725")
 color2 <- c("#79D151", "#29788E")
 
-plot_date <- "02Jul2020"
+plot_date <- "09Jul2020"
 
 # Prior/posterior distributions from abc --------------------------------------
 # Data needs to be transformed to be one vector of values labeled with which
 # parameter it is and which distribution it's from.
 
-# For prior distribution:
-# Create array with one column for label, one for the parameter
-prior_sd <- cbind(model_params$speed, rep("speed", length(model_params$speed)))
-prior_vs <- cbind(model_params$vision, rep("vision", length(model_params$vision)))
-prior_sp <- cbind(model_params$separation, rep("separation", length(model_params$separation)))
-prior_co <- cbind(model_params$cohere, rep("cohere", length(model_params$cohere)))
-prior_sep <- cbind(model_params$separate, rep("separate", length(model_params$separate)))
-prior_mt <- cbind(model_params$match, rep("match",  length(model_params$match)))
+# Reshape dataframes to fit what's needed for ggplot2
+priors <- melt(model_params)  
+priors <- cbind(priors, rep("prior", length(priors$value)))
+colnames(priors) <- c("parameter", "value", "distribution")
 
-# Stack all into one dataframe
-priors <- as.data.frame(rbind(prior_sd, prior_vs, prior_sp, prior_co, prior_sep, prior_mt))
-# Combine values and parameter label in one column with distribution label
-priors <- cbind(priors, rep("prior", length(priors$V1)))
-colnames(priors) <- c("value", "parameter", "distribution")
-
-# Repeat for posterior distribution
 post_all <- as.data.frame(shoaling.abc$unadj.values)
-post_sd <- cbind(post_all$speed, rep("speed", length(post_all$speed)))
-post_vs <- cbind(post_all$vision, rep("vision", length(post_all$vision)))
-post_sp <- cbind(post_all$separation, rep("separation", length(post_all$separation)))
-post_co <- cbind(post_all$cohere, rep("cohere", length(post_all$cohere)))
-post_sep <- cbind(post_all$separate, rep("separate", length(post_all$separate)))
-post_mt <- cbind(post_all$match, rep("match", length(post_all$match)))
-
-posts <- as.data.frame(rbind(post_sd, post_vs, post_sp, post_co, post_sep, post_mt))
-posts <- cbind(posts, rep("posterior", length(posts$V1)))
-colnames(posts) <- c("value", "parameter", "distribution")
+posts <- melt(post_all)
+posts <- cbind(posts, rep("posterior", length(posts$value)))
+colnames(posts) <- c("parameter", "value", "distribution")
 
 dists <- rbind(priors, posts)  # Combine everything into one dataframe
 
 # Make sure all columns will be recongised appropriately for R/ggplot
 dists$value <- as.numeric(as.character(dists$value))
-dists$parameter <- as.factor(dists$parameter)
-dists$distribution <- as.factor(dists$distribution)
 
 # Plot distributions as boxplots
 dist_boxplot <- ggplot(dists, aes(x = parameter, y = value, fill = distribution)) +
@@ -59,8 +41,6 @@ dist_boxplot <- ggplot(dists, aes(x = parameter, y = value, fill = distribution)
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank()) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  # scale_color_viridis(discrete = TRUE, direction = -1) +
-  # scale_fill_viridis(discrete = TRUE, direction = -1) +
   facet_wrap(~parameter, scale="free")
 
 pdf(paste0("~/Desktop/dist_boxplot_", plot_date, ".pdf"))
@@ -74,8 +54,6 @@ dist_density <- ggplot(dists, aes(x = value, fill = distribution, color = distri
   scale_color_manual(values = color2) +
   geom_density(alpha = 0.5) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  # scale_color_viridis(discrete = TRUE, direction = -1) +
-  #scale_fill_viridis(discrete = TRUE, direction = -1) +
   facet_wrap(~parameter, scale="free")
 
 pdf(paste0("~/Desktop/dist_density_", plot_date, ".pdf"))
@@ -84,22 +62,16 @@ dev.off()
 
 
 # Cross-validation plots of true vs. estimated parameter values ---------------
-cv_true <- as.data.frame(shoaling.cv$true)
-cv_estim <- as.data.frame(shoaling.cv$estim)
-colnames(cv_estim) <- c("speed", "vision", "separation", "cohere", "separate", "match")
+# Data from cross-validation output needs to be reshaped to be plotted with ggplot2
+cv_true_raw <- as.data.frame(shoaling.cv$true)
+cv_true <- melt(cv_true_raw)
 
-# Combine all true & estimated parameter values into one dataframe w/3 columns
-cv_sd <- cbind(rep("speed", length(cv_true$speed)), cv_true$speed, cv_estim$speed)
-cv_vs <- cbind(rep("vision", length(cv_true$vision)), cv_true$vision, cv_estim$vision)
-cv_sp <- cbind(rep("separation", length(cv_true$separation)), cv_true$separation, cv_estim$separation)
-cv_co <- cbind(rep("cohere", length(cv_true$cohere)), cv_true$cohere, cv_estim$cohere)
-cv_sep <- cbind(rep("separate", length(cv_true$separate)), cv_true$separate, cv_estim$separate)
-cv_mt <- cbind(rep("match", length(cv_true$match)), cv_true$match, cv_estim$match)
+cv_estim_raw <- as.data.frame(shoaling.cv$estim)
+colnames(cv_estim_raw) <- c("speed", "vision", "separation", "cohere", "separate", "match")
+cv_estim <- melt(cv_estim_raw)
 
-cv_all <- as.data.frame(rbind(cv_sd, cv_vs, cv_sp, cv_co, cv_sep, cv_mt))
+cv_all <- cbind(cv_true, cv_estim$value)
 colnames(cv_all) <- c("parameter", "true", "estimated")
-cv_all$true <- as.numeric(as.character(cv_all$true))
-cv_all$estimated <- as.numeric(as.character(cv_all$estimated))
 
 cv_plots <- ggplot(cv_all, aes(x = true, y = estimated)) + #select data, include color-coding
   theme_bw() +
@@ -113,4 +85,3 @@ cv_plots <- ggplot(cv_all, aes(x = true, y = estimated)) + #select data, include
 pdf(paste0("~/Desktop/cv_plots_", plot_date, ".pdf"))
 print(cv_plots)
 dev.off()
-
