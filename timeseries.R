568562c85c315ed78e7be4f969f567fa46315879
Sophia Wassermann <hi@sowasser.com>
2020-07-21 19:03:17 +0100

    Added timeseries for data from parameter values from NND-only ABC

--- timeseries.R
+++ timeseries.R
@@ -5,24 +5,28 @@ library(ggplot2)
 library(ggcorrplot)
 
 color2_2 <- c("#440154", "#29788E")
+color3 <- c("#440154", "#29788E", "#79D151")
 
 path <- "~/Desktop/DO NOT ERASE/1NUIG/Mackerel/Mackerel Data/"  # for laptop
 
-tracking <- read.csv(paste0(path, "stepwise_data_scaled.csv"))
+tracking_scaled <- read.csv(paste0(path, "stepwise_data_scaled.csv"))
 
 model_run_all <- read.csv(paste0(path, "single_run.csv"))
 
+model_run_all_nnd <- read.csv(paste0(path, "single_run_nnd.csv"))
+
 
 # Timeseries ------------------------------------------------------------------
-model_run <- model_run_all[100:198,]  # only keep 99 steps so same length as tracking
+model_run <- model_run_all[100:198,]  # only keep 99 steps so same length as tracking_scaled
+model_run_nnd <- model_run_all_nnd[100:198,]
 
 steps <- c(1:99)  
 
 # Reformat and stack to make graphs
-tp <- cbind(steps, tracking$polar, rep("polarization", length(tracking$polar)))
-tn <- cbind(steps, tracking$nnd, rep("nearest neighbour distance", length(tracking$nnd)))
-tc <- cbind(steps, tracking$cent, rep("distance from centroid", length(tracking$cent)))
-ta <- cbind(steps, tracking$area, rep("shoal area", length(tracking$area)))
+tp <- cbind(steps, tracking_scaled$polar, rep("polarization", length(tracking_scaled$polar)))
+tn <- cbind(steps, tracking_scaled$nnd, rep("nearest neighbour distance", length(tracking_scaled$nnd)))
+tc <- cbind(steps, tracking_scaled$cent, rep("distance from centroid", length(tracking_scaled$cent)))
+ta <- cbind(steps, tracking_scaled$area, rep("shoal area", length(tracking_scaled$area)))
 
 track_all <- cbind(rbind(tp, tn, tc, ta), rep("tracked", length(396)))
 
@@ -31,9 +35,16 @@ mn <- cbind(steps, model_run$nnd, rep("nearest neighbour distance", length(model
 mc <- cbind(steps, model_run$cent, rep("distance from centroid", length(model_run$cent)))
 ma <- cbind(steps, model_run$area, rep("shoal area", length(model_run$area)))
 
-model_all <- cbind(rbind(mp, mn, mc, ma), rep("modelled", length(396)))
+model_all <- cbind(rbind(mp, mn, mc, ma), rep("modelled (general)", length(396)))
 
-timeseries_all <- as.data.frame(rbind(track_all, model_all))
+# Just for NND, data from the parameters derived from the NND-only ABC
+nndn <- cbind(steps, 
+              model_run_nnd$nnd, 
+              rep("nearest neighbour distance", length(model_run_nnd$nnd)), 
+              rep("modelled (nnd-only)", length(396)))
+
+
+timeseries_all <- as.data.frame(rbind(track_all, model_all, nndn))
 colnames(timeseries_all) <- c("step", "value", "statistic", "source")
 timeseries_all$value <- as.numeric(as.character(timeseries_all$value))
 timeseries_all$step <- as.numeric(as.character(timeseries_all$step))
@@ -41,7 +52,7 @@ timeseries_all$step <- as.numeric(as.character(timeseries_all$step))
 # Graphs of timeseries
 time_graphs <- ggplot() + 
   theme_bw() + 
-  scale_color_manual(values = color2_2) +
+  scale_color_manual(values = color3) +
   geom_line(data=timeseries_all, aes(x=step, y=value, color = source), size = 0.5) +  # line
   xlab("step") +
   ylab("statistic") +
