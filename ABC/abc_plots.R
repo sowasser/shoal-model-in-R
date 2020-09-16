a9aeb163e5c78799198e2fbc34177d116ee1a456
Sophia Wassermann <hi@sowasser.com>
2020-09-16 11:47:21 +0100

    Fixed a miss-labeled variable

--- ABC/abc_plots.R
+++ ABC/abc_plots.R
@@ -96,7 +96,7 @@ raw_coverage <- melt(raw, id="tol")
 # Plot coverage test outcomes
 coverage_hist <- ggplot() +
   theme_bw() +
-  geom_histogram(data = raw3, aes(x = value), bins = 10, color = "#29788E", fill = "#87b7c4") +
+  geom_histogram(data = raw_coverage, aes(x = value), bins = 10, color = "#29788E", fill = "#87b7c4") +
   xlab("tolerance (p value)") +
   ylab("number") +
   ylim(0, 400) +
