527b4dec15b086794036a442b2b8380e59f316e3
Sophia Wassermann <hi@sowasser.com>
2020-11-28 15:04:22 +0000

    Added easy/medium/hard echogram comparisons & means for easy access

--- Survey/odds_ratios.R
+++ Survey/odds_ratios.R
@@ -30,23 +30,38 @@ exp_non.r13 <- odds.ratio(exp[13], nonexp[13])
 exp_non.ratios <- c(exp_non.r1, exp_non.r2, exp_non.r3, exp_non.r4, exp_non.r5, 
                     exp_non.r6, exp_non.r7, exp_non.r8, exp_non.r9, exp_non.r10, 
                     exp_non.r11, exp_non.r12, exp_non.r13)
-t.test(exp_non.ratios, mu = 1)  # p = 0.1051, set the mean at 1 instead of 0
+
+# Run t-test with mean to test at 1 instead of 0
+t.test(exp_non.ratios, mu = 1)  # p = 0.1051, mean = 1.429627
 
 # blue whiting
 exp_non.whb <- c(exp_non.r2, exp_non.r3, exp_non.r7)
-t.test(exp_non.whb, mu = 1)  # p = 0.01913
+t.test(exp_non.whb, mu = 1)  # p = 0.01913, mean = 1.698624 ***
 
 # boarfish 
 exp_non.bof <- c(exp_non.r4, exp_non.r11)
-t.test(exp_non.bof, mu = 1)  # p = 0.3268
+t.test(exp_non.bof, mu = 1)  # p = 0.3268, mean = 1.677139
 
 # Herring 
 exp_non.her <- c(exp_non.r1, exp_non.r5, exp_non.r6, exp_non.r9)
-t.test(exp_non.her, mu = 1)  # p = 0.486
+t.test(exp_non.her, mu = 1)  # p = 0.486, mean = 1566292
 
 # Mackerel 
 exp_non.mac <- c(exp_non.r10, exp_non.r12)
-t.test(exp_non.mac, mu = 1)  # p = 0.9097
+t.test(exp_non.mac, mu = 1)  # p = 0.9097, mean = 0.875
+
+# Easy
+exp_non.easy <- c(exp_non.r1, exp_non.r2, exp_non.r9)
+t.test(exp_non.easy, mu = 1)  # p = 0.3737, mean = 2.011724
+
+# Medium
+exp_non.med <- c(exp_non.r3, exp_non.r4, 
+                 exp_non.r11, exp_non.r12, exp_non.r13)
+t.test(exp_non.med, mu = 1)  # p = 0.02005, mean = 1.571573 ***
+
+# Hard
+exp_non.hard <- c(exp_non.r5, exp_non.r6, exp_non.r7, exp_non.r8)
+t.test(exp_non.hard, mu = 1)  # p = 0.5308, mean = 1.173029
 
 
 # Odds ratio for expert/local expertise correct species ID --------------------
@@ -89,6 +104,19 @@ t.test(exp_local.her, mu = 1)  # p = 0.4171
 exp_local.mac <- c(exp_local.r10, exp_local.r12)
 t.test(exp_local.mac, mu = 1)  # p = 0.9423
 
+# Easy
+exp_local.easy <- c(exp_local.r1, exp_local.r2, exp_local.r9)
+t.test(exp_local.easy, mu = 1)  # p = 0.3239, mean = 1.575181
+
+# Medium
+exp_local.med <- c(exp_local.r3, exp_local.r4, 
+                 exp_local.r11, exp_local.r12, exp_local.r13)
+t.test(exp_local.med, mu = 1)  # p = 0.7348, mean = 1.086118
+
+# Hard
+exp_local.hard <- c(exp_local.r5, exp_local.r6, exp_local.r7, exp_local.r8)
+t.test(exp_local.hard, mu = 1)  # p = 0.9102, mean = 1.030724
+
 
 # Odds ratio for expert/no expertise correct species ID -----------------------
 none <- correct$none.ratio
@@ -129,3 +157,17 @@ t.test(exp_none.her, mu = 1)  # p = 0.3884
 # Mackerel 
 exp_none.mac <- c(exp_none.r10, exp_none.r12)
 t.test(exp_none.mac, mu = 1)  # p = 0.716
+
+# Easy
+exp_none.easy <- c(exp_none.r1, exp_none.r2, exp_none.r9)
+t.test(exp_none.easy, mu = 1)  # p = 0.1276, mean = 2.923395
+
+# Medium
+exp_none.med <- c(exp_none.r3, exp_none.r4, 
+                 exp_none.r11, exp_none.r12, exp_none.r13)
+t.test(exp_none.med, mu = 1)  # p = 0.07524, mean = 2.457693 
+
+# Hard
+exp_none.hard <- c(exp_none.r5, exp_none.r6, exp_none.r7, exp_none.r8)
+t.test(exp_none.hard, mu = 1)  # p = 0.1211, mean = 0.4972452
+
